3d107a57f085dc798551c27af2ad6bb5846764fd
Lucas8x <lucas.poring99@gmail.com>
2020-10-15 11:39:07 -0300

    Aula 4

---
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+
+export default function OrphanageDetails() {
+  return (
+    <View>
+
+    </View>
+  )
+}
